b09c1517116d75b7d10a8c49ddb2dc2079d03e5a
daviidy <yaodavidarmel@gmail.com>
2021-03-25 09:32:46 +0000

    Prepared the directory structure and implemented App component

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
+
+const App = () => (
+  <div>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
+export default App;
