ef091c81c0d2da21ce8fc09df4513d91c6e26b4b
enaburekhan <ericenaburekhan2018@gmail.com>
2021-03-26 11:16:38 +0100

    Create handleChange function in BooksForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [book, setBook] = useState({
+    title: '',
+    category: '',
+  });
 
+  const handleChange = e => {
+    e.preventDefault();
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const
   return (
     <form>
-      <input type="text" id="title" />
-      <select name="category" id="category">
+      <input type="text" id="title" name="title" onChange={handleChange} />
+      <select name="category" id="category" onChange={handleChange}>
 
         {categories.map(val => <option key={val} value={val}>{val}</option>)}
 
       </select>
-      <button type="submit">submit</button>
+      <button type="submit" onClick={handleSubmit}>submit</button>
     </form>
   );
 };
