6b53906321a2420f03900b4ef583fb4d3df2800b
enaburekhan <ericenaburekhan2018@gmail.com>
2021-03-31 15:23:28 +0100

    Create book.category, book.title and comment section markup

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Book = ({ book, handleRemoveBook }) => (
+const Book = ({ book, handleRemoveBook }) => {
   <tr id={book.id}>
     <td>
       { book.id }
@@ -15,7 +15,24 @@ const Book = ({ book, handleRemoveBook }) => (
       <button onClick={() => handleRemoveBook(book)} type="submit">Remove</button>
     </td>
   </tr>
-);
+
+    return (
+      <main className="book-panel">
+        <div>
+          <div className="category book-default-font">{book.category}</div>
+          <div className="title book-default-font">{book.title}</div>
+          <div className="author">Harry Porter</div>
+          <div className="comment-section">
+            <button type="button" className="default-comment">Comments</button>
+            <button className="default-comment" onClick={() => handleRemoveBook(book)} type="submit">Remove</button>
+            <button type="button" className="default-comment">Edit</button>
+          </div>
+        </div>
+        <div />
+        <div />
+      </main>
+    );
+};
 export default Book;
 
 Book.propTypes = {
