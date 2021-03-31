b419ff72ea81cb6ad5edf9a323eee1a5de73b8e5
daviidy <yaodavidarmel@gmail.com>
2021-03-31 11:04:56 +0000

    added navbar component and styled it

---
+++ src/containers/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => (
+  <header className="panel-bg">
+    <nav>
+      <div className="left-navbar">
+        <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+        <p className="BOOKS default-font">Books</p>
+        <p className="CATEGORIES default-font">Categories</p>
+      </div>
+      <div className="Oval">
+        <i className="fas fa-user Mask" />
+      </div>
+    </nav>
+  </header>
+);
+
+export default Navbar;
